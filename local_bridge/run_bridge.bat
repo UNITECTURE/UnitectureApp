1002cfad42c1226bdd935f67c456350a8e969531
RAJWARDHAN-B <rajbravo5613@gmail.com>
2026-02-03 09:46:30 +0530

    random changes

--- local_bridge/run_bridge.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo [%date% %time%] Starting Unitecture Biometric Bridge... >> bridge_log.txt
-:loop
-:: Run Python in unbuffered mode (-u) so logs are written immediately
-python -u bridge.py >> bridge_log.txt 2>&1
-echo [%date% %time%] Bridge crashed or stopped. Restarting in 5 seconds... >> bridge_log.txt
-timeout /t 5
-goto loop
